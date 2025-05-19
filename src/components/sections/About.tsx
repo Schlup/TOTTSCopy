b539a650b68ce67caa93c0206458250e64673526
Schlup <alexschlup25@gmail.com>
2025-05-19 15:18:28 -0300

    style: added alt

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -17,7 +17,7 @@ const About: React.FC = () => {
           </div>
 
           <div className="bg-light-gray p-6 px-12 rounded-2xl shadow-lg text-left">
-            <img className='' src="https://static.wixstatic.com/media/ca5485_dae6beea0cc14014950f2550960c9e69~mv2.png/v1/fill/w_180,h_180,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Livros%201.png" alt="" />
+            <img className='' src="https://static.wixstatic.com/media/ca5485_dae6beea0cc14014950f2550960c9e69~mv2.png/v1/fill/w_180,h_180,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Livros%201.png" alt="Cursos de Prateleira Imagem" />
             <div className="text-2xl font-bold mb-4">
               Cursos de Prateleira
             </div>
@@ -27,7 +27,7 @@ const About: React.FC = () => {
           </div>
 
           <div className="bg-light-gray p-6 px-12 rounded-2xl shadow-lg text-left">
-            <img src="https://static.wixstatic.com/media/ca5485_ed08a34cb636472ebc94a03185d084fe~mv2.png/v1/fill/w_200,h_199,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/%C3%93culos%20VR.png" alt="" />
+            <img src="https://static.wixstatic.com/media/ca5485_ed08a34cb636472ebc94a03185d084fe~mv2.png/v1/fill/w_200,h_199,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/%C3%93culos%20VR.png" alt="Fábrica de Conteúdo Imagem" />
             <div className="text-2xl font-bold mb-4">
               Fábrica de Conteúdo
             </div>
@@ -42,7 +42,7 @@ const About: React.FC = () => {
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
 
           <div className="p-6 px-12 text-center  flex flex-col items-center justify-center">
-            <img loading='lazy' className='w-36' src="https://static.wixstatic.com/media/ca5485_f8c98c6429ee4b8892ccb319e3412db7~mv2.gif" alt="" />
+            <img loading='lazy' className='w-36' src="https://static.wixstatic.com/media/ca5485_f8c98c6429ee4b8892ccb319e3412db7~mv2.gif" alt="Gif exibindo pessoas" />
             <div className="text-orange-600 text-4xl font-bold mb-4 align-middle">
               + de 2,5 milhões
             </div>
@@ -52,7 +52,7 @@ const About: React.FC = () => {
           </div>
 
           <div className="p-6 px-12 text-center  flex flex-col items-center justify-center">
-            <img loading='lazy' className='w-36' src="https://static.wixstatic.com/media/ca5485_54e13942c1654a3193e1f5bca8c2f064~mv2.gif" alt="" />
+            <img loading='lazy' className='w-36' src="https://static.wixstatic.com/media/ca5485_54e13942c1654a3193e1f5bca8c2f064~mv2.gif" alt="Gif exibindo um computador" />
             <div className="text-orange-600 text-4xl font-bold mb-4 align-middle">
               + de 1200
             </div>
@@ -62,7 +62,7 @@ const About: React.FC = () => {
           </div>
 
           <div className="p-6 px-12 text-center  flex flex-col items-center justify-center">
-            <img loading='lazy' className='w-36' src="https://static.wixstatic.com/media/ca5485_ebd3d27817f44f7dbc98816de3cf6fc6~mv2.gif" alt="" />
+            <img loading='lazy' className='w-36' src="https://static.wixstatic.com/media/ca5485_ebd3d27817f44f7dbc98816de3cf6fc6~mv2.gif" alt="Gif exibindo um capelo" />
             <div className="text-orange-600 text-4xl font-bold mb-4 align-middle">
               97%
             </div>
