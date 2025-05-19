50e2639bc68d4bb0ec7037d2ebb7b7201cda7ecd
Schlup <alexschlup25@gmail.com>
2025-05-19 13:30:12 -0300

    style: added icons

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -17,6 +17,7 @@ const About: React.FC = () => {
           </div>
 
           <div className="bg-light-gray p-6 px-12 rounded-2xl shadow-lg text-left">
+            <img className='' src="https://static.wixstatic.com/media/ca5485_dae6beea0cc14014950f2550960c9e69~mv2.png/v1/fill/w_180,h_180,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Livros%201.png" alt="" />
             <div className="text-2xl font-bold mb-4">
               Cursos de Prateleira
             </div>
@@ -26,6 +27,7 @@ const About: React.FC = () => {
           </div>
 
           <div className="bg-light-gray p-6 px-12 rounded-2xl shadow-lg text-left">
+            <img src="https://static.wixstatic.com/media/ca5485_ed08a34cb636472ebc94a03185d084fe~mv2.png/v1/fill/w_200,h_199,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/%C3%93culos%20VR.png" alt="" />
             <div className="text-2xl font-bold mb-4">
               Fábrica de Conteúdo
             </div>
@@ -38,17 +40,20 @@ const About: React.FC = () => {
 
       <div className="container mx-auto pt-24 px-4 sm:px-6 lg:px-8">
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-          <div className="bg-light-gray p-6 px-12 text-center text-orange-600">
-            <div className="text-4xl font-bold mb-4">
-             + de 2,5 milhões
+          
+          <div className="p-6 px-12 text-center  flex flex-col items-center justify-center">
+            <img loading='lazy' className='w-36' src="https://static.wixstatic.com/media/ca5485_f8c98c6429ee4b8892ccb319e3412db7~mv2.gif" alt="" />
+            <div className="text-orange-600 text-4xl font-bold mb-4 align-middle">
+              + de 2,5 milhões
             </div>
             <p className="text-gray-600 text-xl">
               de aprendizes impactados com os nossos cursos.
             </p>
           </div>
 
-          <div className="bg-light-gray p-6 px-12 text-center text-orange-600">
-            <div className="text-4xl font-bold mb-4">
+          <div className="p-6 px-12 text-center  flex flex-col items-center justify-center">
+            <img loading='lazy' className='w-36' src="https://static.wixstatic.com/media/ca5485_54e13942c1654a3193e1f5bca8c2f064~mv2.gif" alt="" />
+            <div className="text-orange-600 text-4xl font-bold mb-4 align-middle">
               + de 1200
             </div>
             <p className="text-gray-600 text-xl">
@@ -56,8 +61,9 @@ const About: React.FC = () => {
             </p>
           </div>
 
-          <div className="bg-light-gray p-6 px-12 text-center text-orange-600">
-            <div className="text-4xl font-bold mb-4">
+          <div className="p-6 px-12 text-center  flex flex-col items-center justify-center">
+            <img loading='lazy' className='w-36' src="https://static.wixstatic.com/media/ca5485_ebd3d27817f44f7dbc98816de3cf6fc6~mv2.gif" alt="" />
+            <div className="text-orange-600 text-4xl font-bold mb-4 align-middle">
               97%
             </div>
             <p className="text-gray-600 text-xl">
